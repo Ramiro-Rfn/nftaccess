edae09eea1b71d3ec87352d40f69709578776fec
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2022-08-29 08:36:44 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+function SignIn(){
+
+  return (
+      <>
+        <Head>
+           <title>Moment | Home</title>
+        </Head>
+        <main className={styles.mainContainer}>
+          NFT Access
+        </main>
+      </>
+      
+  )  
+}
+
+export default SignIn;
